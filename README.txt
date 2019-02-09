6ac3d3121bc52de05dbdff15caf530d319c50146
vvillaplana <vvillaplana@gmail.com>
2019-02-09 15:06:14 +0100

    First commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+A basic application in Javascript to calculate the equivalent age of a dog in human years.
---
+++ app.js
@@ -0,0 +1,13 @@
+//This is my Age
+const myAge = 6;
+//This are the early years of a dog
+let earlyYears = 2;
+earlyYears = earlyYears * 10.5;
+//We susbtract two year from earlyYears to get laterYears
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+const myAgeInDogYears = earlyYears + laterYears;
+const myName = 'Victor'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
